6223e7433de962b0733bb8a0515b6080f0954932
Bridger <bridgermiles3.14@gmail.com>
2024-11-09 10:59:39 -0700

    added a tutorial button

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ class Stack {
     peek(){
         //return this.items[this.items.length-1];
         return this.isEmpty() == true ? "Undefined (The stack is empty it has no values inside.)" : this.items[this.items.length-1];
-        
-        //if stack is empty return either Undefined stack is empty or return nothing
-        //if stack is empty peek should return its empty
     }
 
     isEmpty(){
@@ -92,9 +89,101 @@ document.getElementById('sizeButton').addEventListener('click', function(){
     }, 2000);
 })
 
+document.getElementById('tutorialButton').addEventListener('click', function(){
+
+    let tutVal = document.getElementById('tutVal');
+
+    function wait(ms){
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function waitForButtonClick(buttonId){
+        return new Promise(resolve => {
+            buttonId.addEventListener('click', () => {
+                resolve();
+            })
+        });
+    }
+
+
+
+
+    async function run(){
+    
+    tutVal.innerText = 'A stack data structure is identical to the plates at a buffet';
+
+    await wait(3000);
+    tutVal.innerText = 'Lets populate the stack with three elements.';
+    await wait(3000);
+
+    document.getElementById('pushValue').value = 13;
+    document.getElementById('pushButton').click();
+    tutVal.innerText = '13';
+    await wait(1500);
+
+    document.getElementById('pushValue').value = 28;
+    document.getElementById('pushButton').click();
+    tutVal.innerText = '28';
+    await wait(1500);
+
+    document.getElementById('pushValue').value = 7;
+    document.getElementById('pushButton').click();
+    tutVal.innerText = '7';
+    await wait(1500);
+
+    tutVal.innerText = 'We have three elements in the stack';
+    await wait(3000);
+
+    tutVal.innerText = 'Size will return how many elements are in the stack. Click the .size() button';
+    
+    await waitForButtonClick(sizeButton);
+    tutVal.innerText = 'As you can see the size = 3';
+
+    await wait(3000);
+    tutVal.innerText = 'Click the isEmpty() button.'
+    await waitForButtonClick(isEmptyButton);
+    tutVal.innerText = 'Is empty returns false since the stack is NOT empty.';
+    
+    await wait(3000);
+    tutVal.innerText = 'Click the .peek() button, this button will show you the element that was last added to the stack'
+    await waitForButtonClick(peekButton);
+    tutVal.innerText = 'In this case peek returns 7 since it is the last item we added to the stack';
+    
+
+    await wait(3000);
+    tutVal.innerText = 'click the .pop() button, this will remove and return the top most (recently added) element from the stack.';
+    await waitForButtonClick(popButton);
+    tutVal.innerText = 'Notice the topmost element 7 was removed from the stack and returned.';
+
+    await wait(3000);
+    tutVal.innerText = 'Click the .pop() button again';
+    await waitForButtonClick(popButton);
+    tutVal.innerText = 'Notice the topmost element 28 was removed from the stack and returned.';
+
+    await wait(3000);
+    tutVal.innerText = 'Click the .pop() button one more time';
+    await waitForButtonClick(popButton);
+    tutVal.innerText = 'Notice the topmost element 13 was removed from the stack and returned.';
+
+    await wait(3000);
+    tutVal.innerText = 'Click the isEmpty() button again.'
+    await waitForButtonClick(isEmptyButton);
+    tutVal.innerText = 'Is empty returns TRUE since the stack is empty.';
+
+
+
+
+
+
+
+    }
+    run();
+
+});
+
 //TODO Visuals
-//cneter the number on the stack element
-//make the value input box shorter
+//ceneter the number on the stack element
+//style the input value box
 //have the key value pairs fade
 
 //Expansions add a tutorial button
